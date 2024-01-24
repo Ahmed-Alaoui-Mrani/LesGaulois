0045cf3c752b8552ae3fbe8536fa574a52030f34
Ahmed-Alaoui-Mrani <ahmedalaouimrani@gmail.com>
2024-01-24 02:01:34 +0100

    Classe Gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Gaulois {
+	    private String nom;
+	    private int force;
+
+	    // Constructeur
+	    public Gaulois(String nom, int force) {
+	        this.nom = nom;
+	        this.force = force;
+	    }
+
+	    // Getter pour le nom
+	    public String getNom() {
+	        return nom;
+	    }
+	}
